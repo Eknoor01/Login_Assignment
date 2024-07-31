c7fe086a5e62780571579eef745ad21d1c07c7ee
E_SThunder <140256395+Eknoor01@users.noreply.github.com>
2024-07-31 09:59:23 +0530

    changes

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 function authentication() {
     alert("Hello there! Please add your valid credentials.");
-    var userName = prompt("Enter your username");
-    var password = prompt("Enter your password");
+    var loc=prompt("Enter yor location Choose from Har,Chdor Himachal");
+    if(loc=='Har'|| loc=='Chd' || loc=='Himachal'){
+        var userName = prompt("Enter your username");
+        var password = prompt("Enter your password");
+        if (userName == 'ABC' && password === '1234') {
+            alert("Welcome, " + userName + "!");
+            window.location.href = "signin.html"; 
+        } else {
+            alert("Invalid username or password. Please try again.");
+        }
 
-    if (userName === 'ABC' && password === '1234') {
-        alert("Welcome, " + userName + "!");
-        window.location.href = "signin.html"; 
-    } else {
-        alert("Invalid username or password. Please try again.");
+    }else{
+        alert("We done operate in your state");
     }
+    
 }
